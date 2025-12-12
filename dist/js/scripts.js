6dd8a1a0dea56ed34b7aaeab976aada251023194
aakashv1000 <aakashv1000@gmail.com>
2025-12-12 14:18:03 -0700

    ui changes, updated december

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,3 +1,8 @@
+/*!
+* Start Bootstrap - Resume v7.0.4 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2025 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
 //
 // Scripts
 // 
